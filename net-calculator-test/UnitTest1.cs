da0130ce2aa71f652c12e9c27d69b8c428a7dc95
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-05-09 15:22:14 +0200

    feat: Add e Substract Test

--- net-calculator-test/UnitTest1.cs
+++ net-calculator-test/UnitTest1.cs
@@ -1,3 +1,5 @@
+using net_calculator_tester;
+
 namespace net_calculator_test
 {
     public class Tests
@@ -8,9 +10,40 @@ namespace net_calculator_test
         }
 
         [Test]
-        public void Test1()
+        [TestCase(1.0f, 2.0f)]
+        public void AddTest(float num1 , float num2)
         {
-            Assert.Pass();
+            try
+            {
+                Calcolatrice calcolatrice = new Calcolatrice();
+                float somma = num1 + num2;
+                float result = Calcolatrice.Add(num1, num2);
+
+                Assert.AreEqual(somma, result, $"La somma di {num1} e {num2} deve essere uguale a {somma}");
+            }
+            catch(Exception e) 
+            {
+                Assert.Fail("Eccezione: " + e.Message);
+            }
+        }
+
+
+        [Test]
+        [TestCase(4.0f, 0.0f)]// se inserisce il num 2 più altro andrà in errore
+        public void SubstractTest(float num1, float num2)
+        {
+            try
+            {
+                Calcolatrice calcolatrice = new Calcolatrice();
+                float differenza = num1 - num2;
+                float result = Calcolatrice.Substract(num1, num2);
+
+                Assert.GreaterOrEqual(result, 0, $"La differenza tra {num1} e {num2} deve essere positiva");
+            }
+            catch(Exception e)
+            {
+                Assert.Fail("Eccezione: " + e.Message);
+            }
         }
     }
 }
--- net-calculator-tester/Calcolatrice.cs
+++ net-calculator-tester/Calcolatrice.cs
@@ -20,7 +20,12 @@ namespace net_calculator_tester
 
         public static float Divide(float num1, float num2)
         {
-            return num1 / num2;
+            if (num1 != 0 || num2 != 0)
+                return num1 / num2;
+            else
+            {
+                return 0; 
+            }
         }
 
         public static float Multiply(float num1, float num2)
